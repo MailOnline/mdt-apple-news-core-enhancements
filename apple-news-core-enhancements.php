ba58ab3409fc365e8a4c33c1f6a353c3b0e9f2c0
Michael Ilett <michael.ilett@metro.co.uk>
2022-01-24 11:41:16 +0000

    Resetting loading order

--- apple-news-core-enhancements.php
+++ apple-news-core-enhancements.php
@@ -3,26 +3,26 @@
  * Plugin Name: Apple News Core Enhancements
  * Plugin URI:  https://github.com/MailOnline/mdt-apple-news-core-enhancements/
  * Description: Core enhancements for the publish-to-apple-news plugin
- * Version:     1.0.7
+ * Version:     0.0.1
  * Author:      Metro.co.uk
  * Author URI:  https://github.com/MailOnline/mdt-apple-news-auto-retry/graphs/contributors
  */
 
 namespace MDT\Apple_News_Core_Enhancements;
 
+if ( ! class_exists( '\Apple_News' ) || ! class_exists( '\Admin_Apple_Settings' ) ) {
+	return;
+}
+
+require_once( __DIR__ . '/class-gutenberg.php' );
+require_once( __DIR__ . '/class-admin.php' );
+require_once( __DIR__ . '/class-notices.php' );
+require_once( __DIR__ . '/class-filters.php' );
+require_once( __DIR__ . '/class-auto-retry.php' );
+
 add_action(
 	'init',
 	function() {
-		if ( ! class_exists( '\Apple_News' ) || ! class_exists( '\Admin_Apple_Settings' ) ) {
-			return;
-		}
-
-		require_once( __DIR__ . '/class-gutenberg.php' );
-		require_once( __DIR__ . '/class-admin.php' );
-		require_once( __DIR__ . '/class-notices.php' );
-		require_once( __DIR__ . '/class-filters.php' );
-		require_once( __DIR__ . '/class-auto-retry.php' );
-
 		Notices::init();
 		Filters::init();
 		Auto_Retry::init();
